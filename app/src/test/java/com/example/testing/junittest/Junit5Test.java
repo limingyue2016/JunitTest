2cc4aff1372084a25e495fd6b71435c7ed575d17
李明月 <limingyue5@xiaomi.com>
2020-11-09 10:37:29 +0800

    基础注解测试

--- app/src/test/java/com/example/testing/junittest/Junit5Test.java
+++ app/src/test/java/com/example/testing/junittest/Junit5Test.java
@@ -1,7 +1,13 @@
 package com.example.testing.junittest;
 
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,10 +23,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * 2 lambda表达式 done
  */
 
-public class junit5Test extends BaseCase {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@IncludeTags("calculator")
+public class Junit5Test extends BaseCase {
     private Calculator calculator = new Calculator();
 
     @Test
+    @Disabled
     void testCalculator() {
         assertAll("testCalculator",
                 () -> assertEquals(calculator.add(1, 1), 2),
@@ -28,6 +37,35 @@ public class junit5Test extends BaseCase {
                 () -> assertEquals(calculator.multiply(1, 1), 1),
                 () -> assertEquals(calculator.divide(1, 1), 1),
                 () -> assertEquals(calculator.count, 4));
+
     }
+
+    @Test
+    @Order(4)
+    @Tag("calculator")
+    void testAdd() {
+        assertEquals(calculator.add(1, 1), 2);
+    }
+
+    @Test
+    @Order(3)
+    @Tag("calculator")
+    void testSubtract() {
+        assertEquals(calculator.subtract(1, 1), 0);
+    }
+
+    @Test
+    @Order(2)
+    @RepeatedTest(3)
+    void testMultiply() {
+        assertEquals(calculator.multiply(1, 1), 1);
+    }
+
+    @Test
+    @Order(1)
+    void testDivide() {
+        assertEquals(calculator.divide(1, 1), 1);
+    }
+
 
 }
